428c1c2ca81dba402344e8cdb80a0968f12ab7e1
roberto <robertoiribarren020@gmail.com>
2024-12-12 21:36:18 -0600

    commit inicial

---
+++ CasoEstudio2_G4/CasoEstudio2_G4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CasoEstudio2_G4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
